4a66c2890ec588a69a5625ddcc5ea03ef6ddcd7e
Christian Rhodes <rhodeschristian@icloud.com>
2021-07-11 19:59:38 -0700

    ECD-003-Add-dropdown-menu-to-author-page

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ function backToChoice() {
     document.getElementById("loginBtn").style.display="none";
     document.getElementById("backLink").style.display="none";
 }
+
+function myDropdownMenu() {
+    document.getElementById("myDropdown").classList.toggle("show");
+  }
+  
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('.dropbtn')) {
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
