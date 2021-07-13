f7eb20cb8bf26f0d51c099d15f030172711929b0
Christian Rhodes <rhodeschristian@icloud.com>
2021-07-12 23:09:19 -0700

    ECD-004-Edit-Guest-and-Author-Pages

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 function myLoginButton() {
-    document.getElementById("loginBtn").style.display="block";
-    document.getElementById("backLink").style.display="block";
-    document.getElementById("guestOrAuthor").style.display="none";
+  document.getElementById("loginBtn").style.display="block";
+  document.getElementById("backLink").style.display="block";
+  document.getElementById("guestOrAuthor").style.display="none";
 }
 
 function backToChoice() {
-    document.getElementById("guestOrAuthor").style.display="block";
-    document.getElementById("loginBtn").style.display="none";
-    document.getElementById("backLink").style.display="none";
+  document.getElementById("guestOrAuthor").style.display="block";
+  document.getElementById("loginBtn").style.display="none";
+  document.getElementById("backLink").style.display="none";
 }
 
 function myDropdownMenu() {
-    document.getElementById("myDropdown").classList.toggle("show");
-  }
-  
-  // Close the dropdown menu if the user clicks outside of it
-  window.onclick = function(event) {
-    if (!event.target.matches('.dropbtn')) {
-      var dropdowns = document.getElementsByClassName("dropdown-content");
-      var i;
-      for (i = 0; i < dropdowns.length; i++) {
-        var openDropdown = dropdowns[i];
-        if (openDropdown.classList.contains('show')) {
-          openDropdown.classList.remove('show');
-        }
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
       }
     }
   }
+}
